38befa0f4634682a32aebd1da87ea947b8d3643f
Voravit Euavatanakorn <voravite@yahoo.com>
2018-03-06 06:35:16 +0700

    aaa

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,54 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var line = require('@line/bot-sdk');
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express.Router()
 
-require('dotenv').config();
 
-var app= express.Router();
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
-var config = {
-  channelAccessToke: process.env.channelAccessToken,
-  channelSecret: process.env.channelSecret
-};
-
-var client = new line.Client(config);
-
-app.post('/webhook', line.middleware(config), (req,res,next)=>{
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result)=> res.json(result));
-});
-
-function handleEven(event){
-  console.log(event);
-  if(event.type === 'message' && event.message.type === 'text'){
-    handlemessageEvent(event);
-  }else{
-    return Promise.resolve(null);
+app.post('/webhook', (req, res, next) {
+  var text = req.body.events[0].message.text
+  var sender = req.body.events[0].source.userId
+  var replyToken = req.body.events[0].replyToken
+  console.log(text, sender, replyToken)
+  console.log(typeof sender, typeof text)
+  // console.log(req.body.events[0])
+  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+    sendText(sender, text)
   }
-}
+  res.sendStatus(200)
+})
 
-function handleMessageEvent(event){
-  var msg = {
-    type: 'text',
-    text: 'สวัสดีครับ'
-  };
-  return client.replyMessage(event.replyToken, msg);
+function sendText (sender, text) {
+  let data = {
+    to: sender,
+    messages: [
+      {
+        type: 'text',
+        text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞'
+      }
+    ]
+  }
+  request({
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer bx3HiabWCUvB9HAeXc1SLcJzP7iz6HXoOz2EDrg58Dm/2+U1eUjx8drZPU/+seMpE3pGkwXx62dY8CBU1oDQv0MhMZ6dEIPXyKQnhINJUeLrnyv2lvoYS9s0EUSliDLwtM+HnmLtrp03uRAiMWaQMwdB04t89/1O/w1cDnyilFU='
+    },
+    url: 'https://api.line.me/v2/bot/message/push',
+    method: 'POST',
+    body: data,
+    json: true
+  }, function (err, res, body) {
+    if (err) console.log('error')
+    if (res) console.log('success')
+    if (body) console.log(body)
+  })
 }
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
 
 /* GET home page. */
-app.get('/', function(req, res, next) {
-  res.sendStatus(200)
+app.get('/', function (req, res, next) {
+  res.send('Hello LINE BOT')
 });
 
 module.exports = app;
