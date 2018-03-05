f36470142760aaa984486754db0eb4c9f1cf6716
Voravit Euavatanakorn <voravite@yahoo.com>
2018-03-06 03:26:12 +0700

    route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
+
+router.use(bodyParser.json())
+router.set('port', (process.env.PORT || 4000))
+router.use(bodyParser.urlencoded({extended: false}))
+router.use(bodyParser.json())
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Hello');
 });
 
 module.exports = router;
