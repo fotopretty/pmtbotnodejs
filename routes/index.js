be7c5e70d369fc817e24ecf70f182d011f2a8dd9
Voravit Euavatanakorn <voravite@yahoo.com>
2018-03-06 05:50:18 +0700

    yahoo

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,40 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var line = require('@line/bot-sdk');
+
+require('dotenv').config();
+
 var app= express.Router();
+
+var config = {
+  channelAccessToke: process.env.channelAccessToken,
+  channelSecret: process.env.channelSecret
+};
+
+var client = new line.Client(config);
+
+app.post('/webhook', line.middleware(config), (req,res,next)=>{
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result)=> res.json(result));
+});
+
+function handleEven(event){
+  console.log(event);
+  if(event.type === 'message' && event.message.type === 'text'){
+    handlemessageEvent(event);
+  }else{
+    return Promise.resolve(null);
+  }
+}
+
+function handleMessageEvent(event){
+  var msg = {
+    type: 'text',
+    text: 'สวัสดีครับ'
+  };
+  return client.replyMessage(event.replyToken, msg);
+}
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
