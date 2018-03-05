7dfc09db19ce9233185fba3fef6e9e3dad0f5277
Voravit Euavatanakorn <voravite@yahoo.com>
2018-03-06 03:28:40 +0700

    yahoo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var bodyParser = require('body-parser');
 var router = express.Router();
 
 router.use(bodyParser.json())
-router.set('port', (process.env.PORT || 4000))
 router.use(bodyParser.urlencoded({extended: false}))
 router.use(bodyParser.json())
 
